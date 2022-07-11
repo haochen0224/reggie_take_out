77268f3fc3470fc6471fb5e31297dd62453e289c
HaoChen <hchen170@icloud.com>
2022-07-11 15:08:18 -0400

    Day4-05 completed

---
+++ src/test/java/UploadFileTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+
+public class UploadFileTest {
+    @Test
+    public void test1(){
+        String fileName = "testtest.jpg";
+        String suffix = fileName.substring(fileName.lastIndexOf("."));
+        System.out.println(suffix);
+    }
+}
