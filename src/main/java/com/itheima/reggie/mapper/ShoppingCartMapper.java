a4e3d2537d8502941f3f8d2f09bde0e5a67766b1
HaoChen <hchen170@icloud.com>
2022-07-17 15:06:39 -0400

    Day6-09 completed

---
+++ src/main/java/com/itheima/reggie/mapper/ShoppingCartMapper.java
@@ -0,0 +1,13 @@
+package com.itheima.reggie.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.itheima.reggie.entity.ShoppingCart;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * @Author Hao Chen
+ * @Create 2022/7/17 14:17
+ */
+@Mapper
+public interface ShoppingCartMapper extends BaseMapper<ShoppingCart> {
+}
---
+++ src/main/java/com/itheima/reggie/service/ShoppingCartService.java
@@ -0,0 +1,11 @@
+package com.itheima.reggie.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.itheima.reggie.entity.ShoppingCart;
+
+/**
+ * @Author Hao Chen
+ * @Create 2022/7/17 14:18
+ */
+public interface ShoppingCartService extends IService<ShoppingCart> {
+}
